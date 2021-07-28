af3439a0e80e72d637493ea74f01372bfc5457fe
keshav kabra <keshavkabrakk2@gmail.com>
2021-07-28 11:12:21 +0530

    feed and widgets files

---
+++ src/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Feed.css';
+
+export function Feed() {
+    return (
+        <div className="feed">
+            <h2 className="feed_header"> Home</h2>
+        </div>
+    )
+}
+
+
---
+++ src/Widgets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Widgets.css'
+
+export function Widgets() {
+    return (
+        <div className="widgets">
+            <h1>this is Widgets section</h1>     
+        </div>
+    )
+}
+
