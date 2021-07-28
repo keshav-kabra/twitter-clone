a3d7ac472b3d11d8b0174655fe4cc1c17da78e5d
keshav kabra <keshavkabrakk2@gmail.com>
2021-07-28 11:10:50 +0530

    feed and widgets layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React from 'react';
 import { Sidebar } from './Sidebar';
-
+import {Feed} from './Feed';
+import {Widgets} from './Widgets'
 
 class App extends React.Component {
   render(){
     return(
       <div className="app">
-        
         <Sidebar />
+        <Feed />
+        <Widgets />
+
       </div>
     )
     
