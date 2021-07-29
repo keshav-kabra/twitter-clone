d413bca0f8304fe72be5be6496aa34aa2ab008e0
keshav kabra <keshavkabrakk2@gmail.com>
2021-07-29 12:39:14 +0530

    widgets section done

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from '@material-ui/icons/Search'
+import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from 'react-twitter-embed'
 
 export function Widgets() {
     return (
         <div className="widgets">
-            <h1>This is Widgets section</h1>     
-        </div>
+            <div className="widgets_input">    
+                <SearchIcon className="widgets_searchIcon" />
+                <input placeholder="Search" type="text" />
+            </div>
+            <div className="widgets_widgetContainer">
+                <h2>What's happening ??</h2>
+                <TwitterTweetEmbed tweetId = {"1336614228275294208"} />
+
+                <TwitterTimelineEmbed sourceType="profile" screenName="KeshavK50939438" options={{height : 400}} />
+            </div>
+        </div> 
+        
     )
 }
 
